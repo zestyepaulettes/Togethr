9ee9873a58d433dff4d036ae387ed696af95efcc
Deniz Mekik <denizmekik@gmail.com>
2016-02-19 19:23:06 -0800

    update create and event

--- client/app/create/create.js
+++ client/app/create/create.js
@@ -1,26 +1,27 @@
 angular.module('createEvent', [])
-.controller('CreateController', ['$scope', 'Create', function($scope, Create) {
+.controller('CreateController', ['$scope', 'CreateFactory', function($scope, CreateFactory) {
   $scope.master = {};
   $scope.event = {
     date: new Data(2016, 02, 24)
   };
 
+  //Update function is invoked in submit button in create.html
   $scope.update = function(event) {
     $scope.master = angular.copy(event);
-    Create.addEvent($scope.master)
+    CreateFactory.addEvent($scope.master)
     .then(function () {
       $location.path('/events');
     })
   };
-
+  // clears out the form
   $scope.reset = function() {
     $scope.event = angular.copy($scope.master);
   };
 
   $scope.reset();
 }])
-.factory('Create', function() {
-  
+.factory('CreateFactory', function() {
+  //send a post request to server
   var addEvent = function(event) {
     return $http({
       method: 'POST',
