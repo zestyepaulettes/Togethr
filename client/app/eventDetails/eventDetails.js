6f98f9e182944f1b5b81ceb4c98cc59238d18606
Deniz Mekik <denizmekik@gmail.com>
2016-02-22 17:02:53 -0800

    make request for a specific event

--- client/app/eventDetails/eventDetails.js
+++ client/app/eventDetails/eventDetails.js
@@ -1,3 +1,18 @@
-angular.module('eventDetails', [])
-.controller('eventDetailsController', ['$scope', function($scope) {
+angular.module('eventDetails', ['eventList'])
+.controller('eventDetailsController', ['$scope', 'eventDetailsFactory','EventFactory', 'storeFactory', function($scope, eventDetailsFactory, EventFactory, storeFactory) {
+  $scope.details = {};
+  var initializeDetails = function() {
+    EventFactory.getEvents(storeFactory.eventId)
+      .then(function(details) {
+      	$scope.details = details;
+      })
+      .catch(function(err) {
+      	console.error("eventDetails", err)
+      })
+  	
+  }
+
 }])
+.factory('eventDetailsFactory', function() {
+	
+})
--- client/app/events/events.js
+++ client/app/events/events.js
@@ -1,5 +1,5 @@
 angular.module('eventList', [])
-.controller('EventsController', ['$scope', 'EventFactory', '$location', function($scope, EventFactory, $location) {
+.controller('EventsController', ['$scope', 'EventFactory', '$location','storeFactory', function($scope, EventFactory, $location, storeFactory) {
   $scope.data = {};
   var initializeEvents = function() {
   	EventFactory.getEvents()
@@ -12,17 +12,19 @@ angular.module('eventList', [])
   }
 
   $scope.getEvent = function( event ) {
-  	$location.path('/eventDetails');
+    storeFactory.event = event;
+    $location.path('/eventDetails');
   }
 
   initializeEvents();
 
 }])
 .factory('EventFactory', function($http) {
-	var getEvents = function() {
-		return $http({
+	var getEvents = function(event) {
+		event = event || '';
+    return $http({
 			method: 'GET',
-			url: '/api/events'
+			url: '/api/events/' + event
 		})
 		.then(function(resp) {
 			return resp.data;
@@ -30,5 +32,11 @@ angular.module('eventList', [])
 	}
   return {
     getEvents: getEvents
+  }
+})
+.factory('storeFactory', function() {
+  var eventId = '';
+  return {
+    eventId: eventId
   }
 })
