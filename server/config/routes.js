fa4b45213b2e0a7353745b40fe46275c9177d397
Rhombus33 <diamond.d.wheeler@gmail.com>
2016-02-24 12:03:41 -0800

    Fix GET events/:userID endpoint and modified database schemas  Changes to be committed: 	modified:   server/config/routes.js 	modified:   server/controllers/eventsController.js 	modified:   server/models/models.js 	modified:   server/queries/eventQueries.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -8,15 +8,15 @@ var eventsController = require('../controllers/eventsController');
 // router.get('/users', usersController.getAll);
 // router.post('/users', usersController.addOne);
 
-router.get('/events/:facebookID', eventsController.events.get);
+router.get('/events/:userID', eventsController.events.get);
 router.post('/events', eventsController.events.post);
 router.get('/eventDetails', eventsController.eventDetails.get);
 
 // router.get('/guests', guestsController.getAll);
 // router.post('/guests', guestsController.addOne);
 
-// router.get('/sets', basketsController.getAll);
-// router.post('/sets', basketsController.addOne);
+// router.get('/baskets', basketsController.getAll);
+// router.post('/baskets', basketsController.addOne);
 
 // router.get('/items', itemsController.getAll);
 // router.post('/items', itemsController.addOne);
--- server/controllers/eventsController.js
+++ server/controllers/eventsController.js
@@ -1,21 +1,60 @@
 var EventQuery = require('../queries/eventQueries');
+// var GuestQuery = require('../queries/guestQueries');
+// var ItemQuery = require('../queries/itemQueries');
+// var BasketQuery = require('../queries/basketQueries');
 
 module.exports = {
   events: {
     get: function(req, res) {
-      var facebookID = req.params.facebookID;
-      EventQuery.getAll(facebookID, function(events) {
+      var userID = req.params.userID;
+      EventQuery.getAll(userID, function(events) {
         res.json(events);
       })
-    }, 
+    },
     post: function (req, res) {
-      // TODO
-    } 
+      // var data = {
+      //   userID: req.body.userID,
+      //   event: req.body.event,
+      //   guests: req.body.guests,
+      //   items: req.body.items
+      // };
+      // // Add event
+      // EventQuery.addOne(data.userID, event, function(eventID) {
+      //   // Add event's items, currently not assigned to a basket
+      //   ItemQuery.addAll(eventID, data.items);
+      //   // Add event's guests
+      //   GuestQuery.addAll(eventID, data.guests, function(guestIDs) {
+      //     // Add a basket for each guest
+      //     BasketQuery.addAll(guestIDs, function() {
+      //       //TODO: figure out what to send back
+      //       res.send(''); 
+      //     });
+      //   }); 
+      // });
+    }
   },
 
   eventDetails: {
     get: function(req, res) {
-      // TODO
+    //   // Pull eventID from request params 
+    //   var eventID = req.params.eventID;
+    //   var data = []; // will hold response data
+    //   EventQuery.getByID(eventID, function(event) {
+    //     data.event = event; // get event
+    //   });
+    //   ItemQuery.getAll(eventID, function(items) {
+    //     data.items = items; // get event items
+    //   });
+    //   GuestQuery.getAll(eventID, function(guests) {
+    //     data.guests = guests; // get event guests
+    //   });
+    //   BasketQuery.getAll(eventID, function(baskets) {
+    //     data.baskets = baskets; // get event baskets
+    //   });
+    //   // return JSON.stringified data in response
+    //   res.json(data);
     }
   }
-}
+};
+
+
