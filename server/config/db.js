9cfd566e6cd9b649656c7f26be4db1b2669a8618
Alex Su <alexsu91@gmail.com>
2016-02-26 20:32:25 -0800

    going back to localhost

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,18 @@
 var Sequelize = require('sequelize');
 
-// module.exports = new Sequelize('BeerAndChip', 'root', '', {
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   define: {
-//    timestamps: false
-//   }
-// });
-
-
-module.exports = new Sequelize('heroku_50c8789c054e9e5', 'b8a52638ac3cd9', 'b95b5984', {
-  host: 'us-cdbr-iron-east-03.cleardb.net',
+module.exports = new Sequelize('BeerAndChip', 'root', '', {
+  host: 'localhost',
   dialect: 'mysql',
   define: {
-    timestamps: false
+   timestamps: false
   }
 });
+
+
+// module.exports = new Sequelize('heroku_50c8789c054e9e5', 'b8a52638ac3cd9', 'b95b5984', {
+//   host: 'us-cdbr-iron-east-03.cleardb.net',
+//   dialect: 'mysql',
+//   define: {
+//     timestamps: false
+//   }
+// });
