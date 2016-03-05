4e5260248e4df4dee5ee31b31665407cd82400c4
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-03-05 11:58:58 -0800

    Add user message for UX in top nav bar

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -2,9 +2,10 @@ angular.module('auth', ['ngCookies'])
 
 .controller('AuthController', function($scope, $window, $location, $cookies, AuthFactory) {
   $scope.user = {};
+  $scope.name = $cookies.get('displayName').split(' ')[0];
 
   $scope.logout = function() {
     AuthFactory.signout();
-  }
+  };
 });
 
